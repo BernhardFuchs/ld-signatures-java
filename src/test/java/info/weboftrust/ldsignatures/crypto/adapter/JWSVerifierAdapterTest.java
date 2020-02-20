9f903b440c0cb16151b2d4b380f8fb0602ad0153
Bernhard Fuchs <bernhard.fuchs@danubetech.com>
2020-02-20 13:10:50 +0100

    refactor: improve readability for Adapter tests

--- src/test/java/info/weboftrust/ldsignatures/crypto/adapter/JWSVerifierAdapterTest.java
+++ src/test/java/info/weboftrust/ldsignatures/crypto/adapter/JWSVerifierAdapterTest.java
@@ -3,7 +3,8 @@ package info.weboftrust.ldsignatures.crypto.adapter;
 import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.JWSAlgorithm;
 import com.nimbusds.jose.JWSHeader;
-import info.weboftrust.ldsignatures.crypto.ByteSigner;
+import com.nimbusds.jose.util.Base64URL;
+import info.weboftrust.ldsignatures.crypto.ByteVerifier;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -17,46 +18,48 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.doThrow;
 
 @ExtendWith(MockitoExtension.class)
-class JWSSignerAdapterTest {
+public class JWSVerifierAdapterTest {
 
     @Mock
-    private ByteSigner byteSigner;
+    private ByteVerifier byteVerifier;
+    @Mock
+    private Base64URL signature;
 
-    private JWSSignerAdapter createTestInstance(JWSAlgorithm algorithm) {
-        return new JWSSignerAdapter(byteSigner, algorithm);
+
+    private JWSVerifierAdapter createTestInstance(JWSAlgorithm algorithm) {
+        return new JWSVerifierAdapter(byteVerifier, algorithm);
     }
 
     @Test
-    @DisplayName("When the signer throws a GeneralSecurityException it should be wrapped with it's message in a JOSEException")
-    void given_signThrowsGeneralSecurityException_then_JOSEExceptionReThrown() throws GeneralSecurityException {
+    @DisplayName("When the verifier throws a GeneralSecurityException it should be wrapped with it's message in a JOSEException")
+    void given_verifyThrowsGeneralSecurityException_then_JOSEExceptionReThrown() throws GeneralSecurityException {
         String anyAlgorithmName = "anyAlgorithm";
         JWSAlgorithm anyAlgorithm = new JWSAlgorithm(anyAlgorithmName);
         JWSHeader anyHeader = new JWSHeader(anyAlgorithm);
 
         String expectedErrorMessage = "errorMessage";
         GeneralSecurityException rootException = new GeneralSecurityException(expectedErrorMessage);
-        doThrow(rootException).when(byteSigner).sign(new byte[]{}, anyAlgorithmName);
 
-        JWSSignerAdapter adapter = createTestInstance(anyAlgorithm);
+        doThrow(rootException).when(byteVerifier).verify(new byte[]{}, null, anyAlgorithmName);
 
+        JWSVerifierAdapter adapter = createTestInstance(anyAlgorithm);
         JOSEException exception = assertThrows(JOSEException.class,
-                () -> adapter.sign(anyHeader, new byte[]{})
+                () -> adapter.verify(anyHeader, new byte[]{}, signature)
         );
         assertEquals(exception.getMessage(), expectedErrorMessage);
     }
 
     @Test
-    void given_unexpectedAlgorithm_when_sign_then_JOSEExceptionThrown() {
+    void given_unexpectedAlgorithm_when_verify_then_JOSEExceptionThrown() {
         JWSAlgorithm unexpectedAlgorithm = new JWSAlgorithm("unexpectedAlgorithm");
         JWSAlgorithm expectedAlgorithm = new JWSAlgorithm("expectedAlgorithm");
 
         JWSHeader header = new JWSHeader(unexpectedAlgorithm);
-        JWSSignerAdapter adapter = createTestInstance(expectedAlgorithm);
+        JWSVerifierAdapter adapter = createTestInstance(expectedAlgorithm);
 
         JOSEException exception = assertThrows(JOSEException.class,
-                () -> adapter.sign(header, new byte[]{})
+                () -> adapter.verify(header, new byte[]{}, signature)
         );
         assertEquals(exception.getMessage(), "Unexpected algorithm: " + unexpectedAlgorithm);
     }
-
 }
